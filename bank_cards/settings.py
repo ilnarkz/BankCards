9015bcdbb86785d57199e97769c7c41843c1e70a
ilnarkz <ilnar-16_04@mail.ru>
2022-12-06 18:17:47 +0300

    fix issues

--- bank_cards/settings.py
+++ bank_cards/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://bankcards-production.up.railway.app/', '*']
 
 
 # Application definition
